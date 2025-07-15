a9a9fe8b62653203b05b2ef23718b90102eb06d6
jazibsalman1 <jazib1341@gmail.com>
2025-07-15 12:07:31 +0500

    iteration of for loop

---
+++ for_loops_iterate.py
@@ -0,0 +1,5 @@
+a ="jazib"
+for i in a :
+    print(i)
+# This code iterates over the string "jazib" and prints each character one by one
+# The variable 'i' takes the value of each character in the string during each iteration.
--- forloops.py
+++ forloops.py
@@ -1,4 +1,8 @@
 for jazib in range(10):
     print(jazib)
+print("new for loop next")
 # This code prints the string "jazib" 10 times using a for loop.
 # The variable 'a' is incremented by 1 in each iteration until it reaches 10.
+for i in range (2, 22 , 2):
+    print(i)
+    #this for loops prints numbers from 2 to 20 in steps of 2.
