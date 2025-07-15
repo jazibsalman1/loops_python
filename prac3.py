dd3dd3732113fe6ff322baafc14a953c3fb7ee1d
jazibsalman1 <jazib1341@gmail.com>
2025-07-15 15:57:13 +0500

    allnumber till n sum now

--- prac3.py
+++ prac3.py
@@ -8,3 +8,12 @@ for i in range(2, a):
      print("number is prime")
 print("Check completed.")
 # This code checks if a number is prime by testing divisibility from 2 to the number minus one. If it finds any divisor, it concludes that the number is not prime; otherwise, it declares the number as prime. The check is completed with a final print statement.
+
+n = int(input("Enter a number: "))
+i=1
+sum = 0
+while (i <= n):
+    sum+=i
+    i+=1
+print(sum)
+# This code calculates the sum of all integers from 1 to n using a while loop. It initializes a sum variable to zero and iterates from 1 to n, adding each integer to the sum. Finally, it prints the total sum.
