36629c7bf6fa5e010c4cf5bbee9f5c9f5928463a
jazibsalman1 <jazib1341@gmail.com>
2025-07-15 11:41:04 +0500

    for loop printing jazib 10 times

---
+++ forloops.py
@@ -0,0 +1,4 @@
+for jazib in range(10):
+    print(jazib)
+# This code prints the string "jazib" 10 times using a for loop.
+# The variable 'a' is incremented by 1 in each iteration until it reaches 10.
--- loopspy.py
+++ loopspy.py
@@ -1,5 +1,5 @@
 list = [1,"jazib",2.5,True]
 a = 0
-while (a< 2):
+while (a< 10):
     print(list[1])
     a += 1
