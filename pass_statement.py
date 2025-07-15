af42808921818247f8d7312c3d2ed68c2800ddfe
jazibsalman1 <jazib1341@gmail.com>
2025-07-15 12:24:38 +0500

    pass statement

---
+++ pass_statement.py
@@ -0,0 +1,9 @@
+for i in range(1000):
+    pass
+i = 1
+while (i<1000):
+    print(i)
+    i+=1
+# This code uses a `for` loop with a `pass` statement, which does nothing for each iteration,
+# and then uses a `while` loop to print numbers from 1 to 999.
+# The `while` loop increments `i` by 1 until it reaches 1000
