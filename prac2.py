9600ab48fab4dbfbd6b131eba64ebefc226f22d3
jazibsalman1 <jazib1341@gmail.com>
2025-07-15 15:28:39 +0500

    practicess

---
+++ prac2.py
@@ -0,0 +1,4 @@
+list=["jazib", "ali", "asad", "waqar", "usman"]
+for name in list:
+    if name.startswith("a"):
+        print(f"hello {name}")
---
+++ practice.py
@@ -0,0 +1,4 @@
+a = int(input("Enter a number: "))
+for i in range(1, 11):
+    print(f"{a} x {i} = {a * i}")
+print("Multiplication table completed.")
