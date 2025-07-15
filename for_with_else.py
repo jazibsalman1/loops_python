667dd620c7a5fd705b43ca6051ef6c0417e33871
jazibsalman1 <jazib1341@gmail.com>
2025-07-15 12:19:45 +0500

    continue statemnet

---
+++ for_with_else.py
@@ -0,0 +1,12 @@
+l=[ 1, 2.5]
+for item in l:
+    print(item)
+else:
+    print("End of iteration")
+# This code iterates over a list containing different data types and prints each item.
+for i in range(100):
+    if i==50:
+       continue
+    print(i)
+    
+# This code iterates over a range of numbers from 0 to 99 and skips the 50th number.
