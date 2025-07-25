fdd50530fc13bc1ebaeae15f7b9d8ba73931aa76
jazibsalman1 <jazib1341@gmail.com>
2025-07-25 20:10:58 +0500

    object as a argument kiu leta ha detailed explain

--- oop.py
+++ oop.py
@@ -1,10 +1,23 @@
 class employee :
     name = "Jazib"
     age = 19
-    salary = 100000
+    salary = 10000000000000
+    def info(self): #by default pa ya object ko as a argument pass krta hai
+        print("My Name is:", self.name, "and my Age is:", self.age, "and my Salary is:", self.salary)
 
 
 jazib = employee()
+jazib.info()
 jazib.brother = "Aaban"
 print(jazib.name ,jazib.age, jazib.salary,jazib.brother)
+
 # This code defines a class `employee` with attributes for name, age, and salary. It creates an instance of the class and prints the attributes of that instance, which are initialized with specific
+
+def getinfo():
+        name = input("Enter your name: ")
+        age = int(input("Enter your age: "))
+        salary = int(input("Enter your salary: "))
+        print("My Name is:",name , "and my Age is:", age, "and my Salary is:", salary)
+        
+getinfo()     
+#values. The `getinfo` function prompts the user for their name, age, and salary, and then prints that information.
