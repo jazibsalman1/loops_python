a7b34b14e13bae393ac83cf0dad6fc99fe21646c
jazibsalman1 <jazib1341@gmail.com>
2025-07-25 18:19:51 +0500

    oop concepts

---
+++ oop.py
@@ -0,0 +1,10 @@
+class employee :
+    name = "Jazib"
+    age = 19
+    salary = 100000
+
+
+jazib = employee()
+jazib.brother = "Aaban"
+print(jazib.name ,jazib.age, jazib.salary,jazib.brother)
+# This code defines a class `employee` with attributes for name, age, and salary. It creates an instance of the class and prints the attributes of that instance, which are initialized with specific
