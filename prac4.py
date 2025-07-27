c6d70cd0a1f0643e9555aac1d47321bebaeec0c8
jazibsalman1 <jazib1341@gmail.com>
2025-07-27 18:48:09 +0500

    another way

--- prac4.py
+++ prac4.py
@@ -1,7 +1,14 @@
 class programmer:
     company = "Google"
     a="jazib"
-    b=1000
+    b=100000000000000
 
 jazib = programmer()
 print("Company:", jazib.company," and Name:", jazib.a, " and Salary:", jazib.b)    
+#another way of doing this is
+class programmers:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+object = programmers("Jazib", 100000000000000000)        
+print("Name:", object.name, " and Salary:", object.salary)
