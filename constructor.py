4c1c8b0e5475b4ab00170a87e88ce71df6749e13
jazibsalman1 <jazib1341@gmail.com>
2025-07-27 18:43:01 +0500

    google employess info

--- constructor.py
+++ constructor.py
@@ -1,3 +1,3 @@
-def __init__(self):
+def __init__(self): #this is a dunder method
         print("This is the constructor without any class")
-# This code defines a constructor that prints a message when an instance of the class is created. It does not take any parameters and does not belong to any class.
+# This code defines a constructor that prints a message when an instance of the class is created. It does not take any parameters and does not belong to any class.     
---
+++ prac4.py
@@ -0,0 +1,7 @@
+class programmer:
+    company = "Google"
+    a="jazib"
+    b=1000
+
+jazib = programmer()
+print("Company:", jazib.company," and Name:", jazib.a, " and Salary:", jazib.b)    
